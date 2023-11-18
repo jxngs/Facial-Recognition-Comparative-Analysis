a2c7d5f89d64d6a29ca2acc8b9a5dac7d3ef19ac
Noah Loewy <noah.loewy@duke.edu>
2023-11-18 12:53:36 -0500

    Update recognition_driver.py

--- recognition_driver.py
+++ recognition_driver.py
@@ -1,21 +1,51 @@
 # this file will run implementations from other files
 #from eigenfaces import Eigenfaces
 from fisherfaces import Fisherfaces
+import matplotlib.pyplot as plt
+import numpy as np
 import yalefaces
-NUM_FEATURES = 300
+from filereader import FileReader
+NUM_FEATURES = 110
 #e = Eigenfaces()
+import json
 from PIL import Image
+bestacc, bestfeat = -1,-1
+accuracies=[0]
 
-images, labels = yalefaces.yale_data(NUM_FEATURES)
+for NUM_FEATURES in range(130,133,5):
+    labels, images = FileReader.readFilesToVectors('/Users/noahloewy/Documents/Facial-Recognition-Comparative-Analysis/lfw-deepfunneled', NUM_FEATURES)
+    
+    """
+    json_filename = "names.json"
+    with open(json_filename, 'r') as json_file:
+        labels = json.load(json_file)
+    images = np.load("imagevectors.npy")
+    """l
 
-f = Fisherfaces(images, labels, NUM_FEATURES)
-#print(f.get_fisherface())
-accuracy = 0
-for i in range(1, 16):
-    num = '0' + str(i) if i < 10 else str(i)
-    pred = f.predict('yalefaces_binary/subject' + num + '.centerlight', 'bin')
-    print('pred', i, pred)
-    if 'subject' + num == pred[0]: accuracy += 1
-print('accuracy', accuracy/15)
+    images = np.array([vector.reshape((NUM_FEATURES, NUM_FEATURES)) for vector in images])
+    f = Fisherfaces(images, labels, NUM_FEATURES)
 
-f.save_face(f.mean_face, 'mean.jpeg')
+    accuracy = 0
+    prevlabel=None
+    for label in set(labels):
+        if label == "Venus_Williams":
+            pred = f.predict('/Users/noahloewy/Documents/Facial-Recognition-Comparative-Analysis/lfw-deepfunneled/' + label + '/' + label + '_0001.jpg')
+            print('pred', label, pred)
+            if label == pred[0]: accuracy += 1
+
+    print('accuracy for ' + str(NUM_FEATURES) + " ", accuracy/len(set(labels)))
+    accuracies.append(accuracy/len(labels))
+x_values = np.arange(len(accuracies)) * 5
+"""plt.plot(x_values,accuracies)
+plt.xlabel('NUM FEATURES')
+plt.ylabel('Accuracy')
+plt.title('Features vs Accuracy')
+plt.show()
+"""
+#f.save_face(f.mean_face, 'mean.jpeg')
+
+
+
+
+
+
