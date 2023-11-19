40056c11d2189c9910de04565876f5971be49213
Noah Loewy <noah.loewy@duke.edu>
2023-11-18 19:16:49 -0500

    Update eigenfaces.py

--- eigenfaces.py
+++ eigenfaces.py
@@ -4,6 +4,8 @@ from sklearn import datasets
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 import cv2
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
 
 face_dataset = datasets.fetch_lfw_people(min_faces_per_person=50)
 IMAGE_SIZE = face_dataset.images[0].shape
@@ -12,7 +14,6 @@ face_dataset.images = [cv2.resize(image, (100, 100)) for image in face_dataset.i
 first_image = face_dataset.images[0]
 img = keras.preprocessing.image.array_to_img(np.expand_dims(first_image * 255, axis=-1))
 
-# filter out images if they're the > 150th image for a class -- don't want too many of some classes
 kept_images = []
 kept_labels = []
 frequencies = {}
@@ -47,13 +48,13 @@ X_train =  images[indices[:cut]]
 X_test = images[indices[cut:]]
 
 y_train = labelnames[indices[:cut]]
-y_test = labelnames[indices[cut:]]
+y_actual = labelnames[indices[cut:]]
 
 n_components = 150
 
 import numpy as np
 
-class CustomPCA:
+class PrincipalComponentsAnalysis:
     def __init__(self, n_components):
         self.n_components = n_components
         self.mean = None
@@ -79,21 +80,34 @@ class CustomPCA:
         whitened_data = projected_data / np.sqrt(self.explained_variances)
         return whitened_data
 
-X_train_reshaped = X_train.reshape((len(X_train), -1))
-X_test_reshaped = X_test.reshape((len(X_test), -1))
+X_train = X_train.reshape((len(X_train), -1))
+X_test = X_test.reshape((len(X_test), -1))
 
 n_components = 150
-custom_pca = CustomPCA(n_components=n_components)
-custom_pca.fit(X_train_reshaped)
+custom_pca = PrincipalComponentsAnalysis(n_components=n_components)
+custom_pca.fit(X_train)
 
-X_train_pca = custom_pca.transform(X_train_reshaped)
-X_test_pca = custom_pca.transform(X_test_reshaped)
+X_training_reduced = custom_pca.transform(X_train)
+X_test_reduced = custom_pca.transform(X_test)
 
-classifier = SVC(kernel='linear', C=1)
-classifier.fit(X_train_pca, y_train)
-# Predict on the test set
-y_pred = classifier.predict(X_test_pca)
-# Evaluate the classifier
-accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
+bestacc=0
+for i in range(5):
+    C=10**i
+    classifier = SVC(kernel='linear', C=1)
+    classifier.fit(X_training_reduced, y_train)
+    y_predicted = classifier.predict(X_test_reduced)
+    accuracy = accuracy_score(y_actual, y_predicted)
+    if accuracy>bestacc:
+        bestC = C
+        bestacc = accuracy
 
+classifier = SVC(kernel='linear', C=bestC)
+classifier.fit(X_training_reduced, y_train)
+y_predicted = classifier.predict(X_test_reduced)
+accuracy = accuracy_score(y_actual, y_predicted)
+
+ConfusionMatrixDisplay.from_estimator(
+    classifier, X_test_reduced, y_actual, xticks_rotation="vertical"
+)
+plt.tight_layout()
+plt.show()
