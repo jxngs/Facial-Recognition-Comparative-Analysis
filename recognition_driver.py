e41207ba1194c94f8ecdd648e0df3b042bb4c57e
Noah Loewy <noah.loewy@duke.edu>
2023-11-18 20:22:57 -0500

    Update recognition_driver.py

--- recognition_driver.py
+++ recognition_driver.py
@@ -1,102 +1,55 @@
-# this file will run implementations from other files
-#from eigenfaces import Eigenfaces
+
+import numpy as np
+from sklearn import datasets
+import cv2
 from fisherfaces import Fisherfaces
-import matplotlib.pyplot as plt
-import numpy as np
-import yalefaces
-NUM_FEATURES = 100
-#e = Eigenfaces()
-from PIL import Image, ImageOps
-from filereader import FileReader
-import numpy as np
-import os
-from filereader import FileReader
-NUM_FEATURES = 110
-#e = Eigenfaces()
-import json
-from PIL import Image
+def fisher_runner(NUM_FEATURES):
+    face_dataset = datasets.fetch_lfw_people(min_faces_per_person=50)
 
-def test_lfw_plot():
-    bestacc, bestfeat = -1,-1
-    accuracies=[0]
+    face_dataset.images = [cv2.resize(image, (NUM_FEATURES, NUM_FEATURES)) for image in face_dataset.images]
+    kept_images = []
+    kept_labels = []
+    frequencies = {}
 
-    for NUM_FEATURES in range(25,26,5):
-        labels, images = FileReader.readFilesToVectors('./lfw-deepfunneled', NUM_FEATURES)
-        
-        """
-        json_filename = "names.json"
-        with open(json_filename, 'r') as json_file:
-            labels = json.load(json_file)
-        images = np.load("imagevectors.npy")
-        """
+    for image, label in zip(face_dataset.images, face_dataset.target):
+        if label not in frequencies:
+            frequencies[label] = 0
+        if frequencies[label] > 150:
+            continue
+        kept_images.append(image)
+        kept_labels.append(label)
+        frequencies[label] += 1
+    
+    images = np.array([vector.reshape((NUM_FEATURES, NUM_FEATURES)) for vector in kept_images])
+    labels = np.asarray(kept_labels)
 
-        images = np.array([vector.reshape((NUM_FEATURES, NUM_FEATURES)) for vector in images])
-        f = Fisherfaces(images, labels, NUM_FEATURES)
+    labeldict = {i: name for i, name in enumerate(face_dataset.target_names)}
+    labelnames = []
+    for i in range(len(labels)):
+        labelnames.append(labeldict[labels[i]])
 
-        accuracy = 0
-        prevlabel=None
-        for label in set(labels):
-            if label == "Venus_Williams":
-                pred = f.predict('./lfw-deepfunneled/' + label + '/' + label + '_0001.jpg', 'jpeg')
-                print('pred', label, pred)
-                if label == pred[0]: accuracy += 1
+    labelnames = np.asarray(labelnames)
 
-        print('accuracy for ' + str(NUM_FEATURES) + " ", accuracy/len(set(labels)))
-        accuracies.append(accuracy/len(labels))
-    x_values = np.arange(len(accuracies)) * 5
-    plt.plot(x_values,accuracies)
-    plt.xlabel('NUM FEATURES')
-    plt.ylabel('Accuracy')
-    plt.title('Features vs Accuracy')
-    plt.show()
+    indices = np.arange(len(images))
+    np.random.seed(18)
+    np.random.shuffle(indices)
 
-#f.save_face(f.mean_face, 'mean.jpeg')
+    prop = .25
+    cut = int((1 - prop) * len(images))//1
 
+    X_train =  images[indices[:cut]]
+    X_test = images[indices[cut:]]
 
-
-
-def test_lfw():
-    if False:
-        labels, images = FileReader.readFilesToVectors('./lfw-deepfunneled')
-        f = Fisherfaces(images, labels, NUM_FEATURES)
-        print(f.get_fisherface())
-        f.save_face(f.get_fisherface(), 'proj/fisher.jpeg')
-    elif True:
-        base_filepath = './lfw-deepfunneled'
-        count = 0
-
-        for person_name in os.listdir(base_filepath):
-
-                if str(person_name).startswith('.'):
-                    continue
-                # get the image files from each folder
-                count += 1
-                if person_name == 'George_W_Bush': print(count)
-                if count > 1900: print(count, person_name)
-                if count == 1910: break
-
-    else:
-        image = np.array(ImageOps.grayscale(Image.open('lfw-deepfunneled/George_W_Bush/George_W_Bush_0001.jpg').resize((NUM_FEATURES, NUM_FEATURES))))
-        fisherface = np.array(ImageOps.grayscale(Image.open('altered_images/proj/fisher.jpeg').resize((NUM_FEATURES, NUM_FEATURES))))
-
-
-
-        f = Fisherfaces(images, labels, NUM_FEATURES)
-        pred = f.predict_with_fisher(fisherface, image)
-        print(pred)
-
-def test_yale(n):
-    images, labels = yalefaces.yale_data(n)
-    f = Fisherfaces(images, labels, n)
+    y_train = labelnames[indices[:cut]]
+    y_actual = labelnames[indices[cut:]]
+    f = Fisherfaces(X_train, y_train, NUM_FEATURES)
 
     accuracy = 0
-    for i in range(1, 16):
-        num = '0' + str(i) if i < 10 else str(i)
-        pred = f.predict('yalefaces_binary/subject' + num + '.centerlight', 'bin')
-        print('pred', i, pred)
-        if 'subject' + num == pred[0]: accuracy += 1
-    print('accuracy', accuracy/15)
+    for x,y in zip(X_test, y_actual):
+        
+        pred = f.predict(x)
+        print('pred', y, pred)
+        if y == pred[0]: accuracy += 1
+    print('accuracy', accuracy/len(X_test))
 
-    # f.save_face(f.mean_face, 'mean.jpeg')
-
-test_yale(15)
+fisher_runner(40)
